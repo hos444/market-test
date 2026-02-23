741ffd7f18a73994cc1e58cabf1d7251c33bc5b8
hos444 <hoszblak@gmail.com>
2026-02-23 20:52:23 +0200

    before class

--- lib/screens/onboarding_screen/onboarding1.dart
+++ lib/screens/onboarding_screen/onboarding1.dart
@@ -6,9 +6,73 @@ class OnBoardin1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('OnBoardin1'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset('assets/logo/Image.png', height: 244, width: 244),
+          SizedBox(height: 40),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: 44,
+                      width: 349,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0, top: 6),
+                        child: Text('Please enter your email'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 39),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: 44,
+                      width: 349,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0, top: 6),
+                        child: Text('Minimum 8 characters required'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -10,6 +10,62 @@ class UserScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text('Profile')),
       bottomNavigationBar: BottomNavBar(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, left: 8, right: 8),
+            // card of account info
+            child: Card(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  //image prodect
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 8.0,
+                      left: 12,
+                      bottom: 12,
+                    ),
+                    child: CircleAvatar(
+                      radius: 40,
+                      child: Image.asset(
+                        'assets/logo/Image.png',
+                        height: 88,
+                        width: 88,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // name prodect
+                      Text(
+                        'account name',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.green,
+                        ),
+                      ),
+
+                      //price
+                      Text(
+                        'Member since (the year) / account type',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
